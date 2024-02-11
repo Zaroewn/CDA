9b7dfc05fdcacca23b80e11788436c4f00f3c203
zaroewn <maxime.lamotte@outlook.com>
2024-02-11 18:46:38 +0100

    ajout commentaires

--- php/post.php
+++ php/post.php
@@ -11,7 +11,27 @@ WHERE P.id = :id');
 $query -> bindValue(':id', $_GET['id'], PDO::PARAM_INT);
 $query -> execute();
 $post = $query -> fetch(PDO::FETCH_ASSOC);
+
+if(empty($post)) {
+
+    http_response_code(404);
+    header('HTTP/1.0 404 Not Found');
+    readfile('../html/404.html');
+    exit();
+
+}
 // var_dump($post);
+
+$query = $pdo -> prepare('SELECT commentaires.corps, commentaires.created_at, utilisateurs.nom, utilisateurs.photo 
+FROM commentaires
+LEFT JOIN utilisateurs
+ON commentaires.id_utilisateur = utilisateurs.id
+WHERE id_post = :id');
+
+$query -> bindValue('id', $_GET['id'], PDO::PARAM_INT);
+$query -> execute();
+$commentaires = $query -> fetchAll(PDO::FETCH_ASSOC);
+
 ?>
 
 <!DOCTYPE html>
@@ -56,6 +76,24 @@ $post = $query -> fetch(PDO::FETCH_ASSOC);
                     "</div>";
             
         ?>
+    </section>
+
+    <section class="commentaires">
+        <h3>Commentaires</h3>
+        <hr><br>
+        <?php foreach($commentaires as $commentaire) : ?>
+                            <div class="commentaire">
+                                <div class="img">
+                                    <img src="../src/<?=$commentaire['photo']?>">
+                                    <div class="info">
+                                        <h5><?=$commentaire['nom']?></h5>
+                                        <span><?=$commentaire['created_at']?></span>
+                                    </div>
+                                </div>
+                                <p><?=$commentaire['corps']?></p>
+                            </div>
+                            <br>
+                    <?php endforeach ?>
     </section>
 
     <footer>
